08f37c5ca80c4b2903d9a7d3022e8a53781521d7
niloofar <mousavi.n0@gmail.com>
2024-11-16 16:33:44 +0100

    add parent directory to sys.path

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add the parent directory to sys.path to make api.py discoverable
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 import unittest
 from api import fetch_weather_data
 
